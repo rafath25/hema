d57742359ba3195f94bea73d156737b221ee48f0
rafath25 <amjathkhanlinux003@gmail.com>
2024-06-05 10:50:31 +0530

    Revert "added third line"
    This reverts commit 6f61318bd0871f2b98fec22a7c0f0f6d9cb478e8.

--- jupiter.rb
+++
@@ -1,5 +0,0 @@
-anks to the world of the internet! We get to see several videos and pictures on a daily basis with global information. A recent incidence included an Indian woman living in Canada who was trying to return an Amazon product when she me
-
-3.hafjsajfajfvjv
-4.adbjasff
-5.svsdndn
--- saturn.py
+++ saturn.py
@@ -1,3 +1,2 @@
 This is dummy script.
 This is second line.:wq
-This i sthird line:
